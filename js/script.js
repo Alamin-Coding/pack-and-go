ff530b10319ec0cf6bef2410c9dcd67c6c215798
Alamin-Coding <alamin.codelover@gmail.com>
2022-11-26 10:45:23 +0600

    Added all files and folder

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$(function(){
+    feather.replace();
+    $('.popular_slider').slick({
+        slidesToShow:3,
+        arrows:false,
+        centerMode: true,
+        centerPadding: "200px",
+        focusOnSelect: true,
+      });
+})

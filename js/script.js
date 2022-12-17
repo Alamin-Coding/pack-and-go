b2e88b7d1d06b010218978a72a89d155714be88b
Alamin-Coding <alamin.codelover@gmail.com>
2022-12-17 12:21:23 +0600

    video add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
 
 $(function(){
+
+  // Menu Fixed
+  let navbar = document.querySelector(".navbar");
+  let back_to_top = document.querySelector(".back_to_top");
+
+
+  window.addEventListener("scroll", function(){
+    let value = window.scrollY;
+    if (value > 250) {
+      navbar.classList.add("menu_fixed");
+      back_to_top.classList.add("show");
+
+    }else{
+      navbar.classList.remove("menu_fixed")
+      back_to_top.classList.remove("show");
+    }
+    // console.log(Math.floor(value));
+  })
+
+  // BG video
+  $('#banner').vide('/video/ocean.mp4');
+
+
+
+
+
+
+
+
     feather.replace();
     $('.popular_slider').slick({
         slidesToShow:3,
@@ -7,5 +36,15 @@ $(function(){
         centerMode: true,
         centerPadding: "200px",
         focusOnSelect: true,
+        dots: true,
       });
+
+
+
+$('.explore_slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
 })
